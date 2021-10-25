aa984c9e6838ce182a25aeae34d82994f875ffbd
akki <akshaybamne1@gmail.com>
2021-10-25 18:36:09 +0530

    Added all files

--- wdio.conf.js
+++ wdio.conf.js
@@ -154,7 +154,7 @@ exports.config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 60000,
+        timeout: 150000,
         // require: ['@babel/register']
         // require: ['@babel/register', './test/helpers/common.js']
         require: ['@babel/register']
